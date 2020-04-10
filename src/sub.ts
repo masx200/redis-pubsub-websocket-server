3aeb3c13f8c70ea4858d515ad8b4dfbf6108dad9
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-10 15:34:45 +0800

    Update sub.ts

--- src/sub.ts
+++ src/sub.ts
@@ -1,6 +1,6 @@
 import ioredis from "ioredis";
 const redisclient = new ioredis({ port: 6379, host: "localhost" });
-redisclient.ping().then(console.log);
+redisclient.ping().then((pong) => console.log("ping", pong));
 export async function subscribe(obj: any) {
     console.log(obj);
 }
