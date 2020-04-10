0a9de18810b079f22ff2bf1561c102eb1b78ff56
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-10 16:20:43 +0800

    Update sub.ts

--- src/sub.ts
+++ src/sub.ts
@@ -1,40 +1,34 @@
-import ws from "ws";
 import ioredis from "ioredis";
+import ws from "ws";
 const redisclient = new ioredis({ port: 6379, host: "localhost" });
 redisclient.ping().then((pong) => console.log("ping", pong));
 redisclient.on("message", function (channel, message) {
-    // Receive message Hello world! from channel news
-    // Receive message Hello again! from channel music
     console.log("Receive message %s from channel %s", message, channel);
 });
-const listensocket = new Map<string, Set<ws>>();
-export async function subscribe(
-    socket: ws,
-    obj: { type: "subscribe"; channel: string }
-) {
-    console.log(socket, obj);
-    const { channel } = obj;
-    redisclient
-        .subscribe(channel)
-        .then(() => console.log("subscribe ", channel));
-    const haschannel = listensocket.get(channel);
+const listensocketmap = new Map<string, Set<ws>>();
+export async function subscribe(socket: ws, channel: string) {
+    console.log(socket, channel);
+
+    const haschannel = listensocketmap.get(channel);
 
     const listeners =
         haschannel ??
         (() => {
             let listenset = new Set<ws>();
-            listensocket.set(channel, listenset);
+            listensocketmap.set(channel, listenset);
             return listenset;
         })();
 
     listeners.add(socket);
+    console.log(listensocketmap);
+    await redisclient
+        .subscribe(channel)
+        .then(() => console.log("subscribe ", channel));
 }
 export async function unsubscribe(
     socket: ws,
-    obj: {
-        type: "unsubscribe";
-        channel: string;
-    }
+
+    channel: string
 ) {
-    console.log(socket, obj);
+    console.log(socket, channel);
 }
