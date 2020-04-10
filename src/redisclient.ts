e5274f656174fe74a4e7e94d8eda1fa11d4e869f
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-10 17:08:30 +0800

    Update redisclient.ts

--- src/redisclient.ts
+++ src/redisclient.ts
@@ -1,6 +1,17 @@
+import ws from "ws";
 import ioredis from "ioredis";
+import { listensocketmap } from "./listensocketmap";
 export const redisclient = new ioredis({ port: 6379, host: "localhost" });
 redisclient.ping().then((pong) => console.log("ping", pong));
 redisclient.on("message", async function (channel, message) {
     console.log("Receive message %s from channel %s", message, channel);
+
+    const listeners = listensocketmap.get(channel);
+    const data = { type: "message", channel, message: JSON.parse(message) };
+    listeners &&
+        listeners.forEach((socket) => {
+            socket.readyState === ws.OPEN
+                ? socket.send(JSON.stringify(data))
+                : listeners.delete(socket);
+        });
 });
