9347c7b1d3c327acb71ef6dcddbfcd4f38a84873
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-10 14:00:40 +0800

    Update ws-handler.ts

--- src/ws-handler.ts
+++ src/ws-handler.ts
@@ -4,23 +4,25 @@ const handle_ws = (socket: ws): void => {
     // console.log(socket);
 
     socket.on("open", () => {
-        socket.send("test");
-        setTimeout(function timeout() {
-            socket.send(Date.now());
-        }, 500);
+        console.log("websocket connected");
+        // socket.send("test");
+        // setTimeout(function timeout() {
+        //     socket.send(Date.now());
+        // }, 500);
     });
     socket.on("error", (error) => {
-        console.log(error);
+        console.log("websocket error", error);
+        // console.log(error);
     });
     socket.on("message", (message) => {
-        console.log("received: %s", message);
-        socket.send(Date.now());
-        setTimeout(function timeout() {
-            socket.send(Date.now());
-        }, 500);
+        console.log("websocket received: ", message);
+        // socket.send(Date.now());
+        // setTimeout(function timeout() {
+        //     socket.send(Date.now());
+        // }, 500);
     });
     socket.on("close", (code, reason) => {
-        console.log({ code, reason });
+        console.log("websocket closed", code, reason);
     });
     socket.emit("open");
 };
