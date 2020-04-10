b363c9845cec8f90bb181ec4f4b29b30e683ce4c
masx200 <masx200@qq.com>
2020-04-10 23:42:25 +0800

    update src/error-response.ts.

--- src/error-response.ts
+++ src/error-response.ts
@@ -1,6 +1,9 @@
-export default function(e:Error){
+import {formaterror}from "./formaterror"
+export default function(error:Error){
+let reason = String(error).split("\n").join("")
+let stack=formaterror(error)
         
-                
+                return {stack,error:reason,type: "error"}
                             
                                             
                                                                 
