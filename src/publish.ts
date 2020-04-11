4802da03bc4432f0dfc222f6db1410bc8e8b6b8d
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-11 15:46:17 +0800

    Update publish.ts

--- src/publish.ts
+++ src/publish.ts
@@ -1,4 +1,8 @@
-export async function publish(channel:string, message:any) {
+import { redisclient } from "./redis-client";
 
-    
+export async function publish(channel: string, message: any) {
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
+    await redisclient.publish(channel, msg).then(() => {
+        console.log("publish success ", channel, message);
+    });
 }
