dd76b9364a258e4ed204e19d3dd4fbf936074385
masx200 <masx200@qq.com>
2020-04-11 21:53:46 +0800

    update test/index.ts.

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,20 @@
-import createpubsub from "./pubsub";
-const suber = new createpubsub({
+import pubsub from "./pubsub";
+const suber = new pubsub({
     port: 2000,
     host: "localhost",
     path: "/websocket",
     protocol: "ws:",
     channels: ["test", "event-127.0.0.1-5000"],
 });
-console.log(createpubsub);
+console.log(pubsub);
 console.log(suber);
-const puber = new createpubsub({
+const puber = new pubsub({
     host: "localhost",
     port: 2000,
     path: "/websocket",
     channels: ["broadcast"],
 });
 console.log(puber);
-const pubsub = new createpubsub({ url: "ws://localhost:2000/websocket" });
+const pubsub = new pubsub({ url: "ws://localhost:2000/websocket" });
 console.log(pubsub);
 [suber, puber, pubsub].forEach((a) => a.channels.add("broadcast"));
