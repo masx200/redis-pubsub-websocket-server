a6634dfcecc1228f5237f435b272317b62225d23
masx200 <masx200@qq.c60>
2020-04-11 23:16:26 +0800

    Update pubsub.ts

--- test/pubsub.ts
+++ test/pubsub.ts
@@ -8,7 +8,7 @@ function checkchannel(channel: string) {
 function definefreeze(instance: object) {
     Object.entries(Object.getOwnPropertyDescriptors(instance)).forEach(
         ([key, olddesc]) => {
-            const shouldwrite = Reflect.has(olddesc, "writeable");
+            const shouldwrite = Reflect.has(olddesc, "writable");
             const descripter = {
                 ...olddesc,
                 configurable: false,
